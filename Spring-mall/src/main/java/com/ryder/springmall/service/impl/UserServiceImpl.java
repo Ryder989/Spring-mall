e216d0f7afcf3ab57f90911563e7a7175d02a6dc
Ryder989 <a0981724831@gmail.com>
2026-04-24 19:09:36 +0800

    商品功能 - 使用 MD5 加密

--- Spring-mall/src/main/java/com/ryder/springmall/service/impl/UserServiceImpl.java
+++ Spring-mall/src/main/java/com/ryder/springmall/service/impl/UserServiceImpl.java
@@ -10,7 +10,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.nio.charset.StandardCharsets;
 
 @Component
 public class UserServiceImpl implements UserService {
@@ -21,6 +24,7 @@ public class UserServiceImpl implements UserService {
     private UserDao userDao;
 
     @Override
+    // 註冊帳號
     public Integer register(UserRegisterRequest userRegisterRequest) {
         // 檢查註冊的email
         User user = userDao.getUserByEmail(userRegisterRequest.getEmail());
@@ -29,6 +33,11 @@ public class UserServiceImpl implements UserService {
             log.warn("該 email {} 已經被註冊",userRegisterRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
+
+        // 使用 MD5 生成密碼的雜湊值
+        String hashPassword = DigestUtils.md5DigestAsHex(userRegisterRequest.getPassword().getBytes());
+        userRegisterRequest.setPassword(hashPassword);
+
         // 創建email
         return userDao.createUser(userRegisterRequest);
     }
@@ -39,14 +48,22 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    // 登入帳號
     public User login(UserLoginRequest userLoginRequest) {
         User user = userDao.getUserByEmail(userLoginRequest.getEmail());
 
+        // 檢查帳號是否存在
         if (user == null){
             log.warn("該 email {} 尚未註冊",userLoginRequest.getEmail());
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
         }
-        if (user.getPassword().equals(userLoginRequest.getPassword())){
+
+        // 使用MD5 生成密碼的雜湊值
+        String hashPassword = DigestUtils.md5DigestAsHex(userLoginRequest.getPassword().getBytes());
+
+
+        // 檢查密碼是否正確
+        if (user.getPassword().equals(hashPassword)){
             return user;
         }else {
             log.warn("email {} 的密碼不正確",userLoginRequest.getPassword());
