b6204c0c70e7e79b0276c96fcd94b4b365e6b04c
Ryder989 <a0981724831@gmail.com>
2026-04-26 13:40:27 +0800

    訂單功能 - 檢查user是否存在、扣除商品庫存

--- Spring-mall/src/main/java/com/ryder/springmall/dao/OrderDao.java
+++ Spring-mall/src/main/java/com/ryder/springmall/dao/OrderDao.java
@@ -13,4 +13,5 @@ public interface OrderDao {
     Order getOrderById(Integer orderId);
 
     List<OrderItem> getOrderItemsByOrderId(Integer orderId);
+
 }
--- Spring-mall/src/main/java/com/ryder/springmall/service/impl/OrderServiceImpl.java
+++ Spring-mall/src/main/java/com/ryder/springmall/service/impl/OrderServiceImpl.java
@@ -2,15 +2,21 @@ package com.ryder.springmall.service.impl;
 
 import com.ryder.springmall.dao.OrderDao;
 import com.ryder.springmall.dao.ProductDao;
+import com.ryder.springmall.dao.UserDao;
 import com.ryder.springmall.dto.BuyItem;
 import com.ryder.springmall.dto.CreateOrderRequest;
 import com.ryder.springmall.model.Order;
 import com.ryder.springmall.model.OrderItem;
 import com.ryder.springmall.model.Product;
+import com.ryder.springmall.model.User;
 import com.ryder.springmall.service.OrderService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,15 +28,37 @@ public class OrderServiceImpl implements OrderService {
     private OrderDao orderDao;
     @Autowired
     private ProductDao productDao;
+    @Autowired
+    private UserDao userDao;
+
+    private final static Logger log = LoggerFactory.getLogger(OrderServiceImpl.class);
 
     @Transactional
     @Override
     public Integer createOrder(Integer userId, CreateOrderRequest createOrderRequest) {
+        User user = userDao.getUserById(userId);
+        if (user == null){
+            log.warn("該 userId {} 不存在",userId);
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        }
+
+
         int totalAmount = 0;
         List<OrderItem> OrderItemList = new ArrayList<>();
 
         for (BuyItem buyItem:createOrderRequest.getBuyItemList()){
             Product product = productDao.getProductById(buyItem.getProductId());
+
+            // 檢查 product 是否存在、庫存是否足夠
+            if (product == null ){
+                log.warn("商品 {} 不存在",buyItem.getProductId());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }else if (product.getStock() < buyItem.getQuantity()){
+                log.warn("商品 {} 庫存量不足，無法購買。剩餘庫存 {} ，欲購買數量 {}",product.getProduct_name(),
+                        product.getStock(),buyItem.getQuantity());
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+            }
+            productDao.updateStock(product.getProduct_id(),product.getStock()-buyItem.getQuantity());
 
             // 計算總金額
             int amount = buyItem.getQuantity() * product.getPrice();
